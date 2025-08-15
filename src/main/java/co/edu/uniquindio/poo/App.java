ec24baa7cb3fcd947deb4fea7011941e343b1866
Byron-Prieto <160172778+Byron-Prieto@users.noreply.github.com>
2025-08-15 08:36:47 -0500

    ejercicio

---
+++ src/main/java/co/edu/uniquindio/poo/App.java
@@ -0,0 +1,12 @@
+package co.edu.uniquindio.poo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        int [] arreglo = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(Arreglo.contarPares (arreglo));
+    }
+}
---
+++ src/main/java/co/edu/uniquindio/poo/Arreglo.java
@@ -0,0 +1,23 @@
+package co.edu.uniquindio.poo;
+
+public class Arreglo {
+
+     public static int contarPares(int[] arreglo) {
+        return contarParesRecursivo(arreglo, 0, arreglo.length - 1);
+    }
+
+    // Método recursivo usando divide y vencerás
+    private static int contarParesRecursivo(int[] arreglo, int inicio, int fin) {
+        if (inicio > fin) {
+            return 0;
+        }
+        if (inicio == fin) {
+            return arreglo[inicio] % 2 == 0 ? 1 : 0;
+        }
+        int medio = (inicio + fin) / 2;
+        int izquierda = contarParesRecursivo(arreglo, inicio, medio);
+        int derecha = contarParesRecursivo(arreglo, medio + 1, fin);
+        return izquierda + derecha;
+    }
+    
+}
